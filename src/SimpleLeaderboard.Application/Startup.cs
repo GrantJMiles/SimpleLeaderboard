ba33f618635454981a245c688287bf42abc0bd48
Grant Miles <grantmiles19@gmail.com>
2022-03-11 21:25:52 +0000

    Converted for Sqlite so that the application can run isolated in a single container

--- src/SimpleLeaderboard.Application/Startup.cs
+++ src/SimpleLeaderboard.Application/Startup.cs
@@ -31,7 +31,7 @@ namespace SimpleLeaderboard.Application
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var conn = Configuration.GetConnectionString("SimpleLeaderboardDb");
+            var conn = Configuration.GetConnectionString("SimpleLeaderboadDb");
             services.AddDbContext<SimpleLeaderboardContext>(options => options.UseSqlite($"Data Source={conn}"));
             // services.AddDbContext<SimpleLeaderboardContext>(options => options.UseSqlServer(conn));
             services.AddControllersWithViews();
